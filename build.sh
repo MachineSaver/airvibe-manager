d5cc9dea9cc1780be5e11c0ddb2a6f1e3785c659
leobach <leo@machinesaver.net>
2026-02-24 16:15:44 -0600

    build.sh: git pull before building so VPS always deploys latest code

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Always deploy the latest committed code.
+git pull
+
 export NEXT_PUBLIC_BUILD_HASH=$(git rev-parse --short HEAD)
 export NEXT_PUBLIC_BUILD_DATE=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 
